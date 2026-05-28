fcc643d3753143fa87c296e9d935bd08474aba80
yasirlabs <mubermij@gmail.com>
2026-05-29 00:38:32 +0300

    update config file

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
             value: [
               "default-src 'self'",
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://partner.googleadservices.com https://www.googletagservices.com https://tpc.googlesyndication.com https://cdn.ampproject.org",
-              "frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://pagead2.googlesyndication.com",
+              "frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://pagead2.googlesyndication.com https://cdn.ampproject.org",
               "img-src 'self' data: blob: https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net",
               "connect-src 'self' https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://yasir723-tejreed.hf.space",
               "style-src 'self' 'unsafe-inline'",
